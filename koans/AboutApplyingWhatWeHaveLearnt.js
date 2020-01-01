5b8a1a4a05b9da62eacc490f5e5288896ee912ac
Chris Hesterman <spoiler14@comcast.net>
2020-01-01 15:48:37 -0800

    Complete code for sum multiples of 5 or 3 function AboutApplying...js
    Comment recommended chaining _.range and _.reduce, which I did, but without
    wrapping them in _.chain and _.value.  Tried it both ways, both were
    successful.  Chose the less verbose format, then stumbled upon a method for
    this using filter between range and reduce.  I did not use filter in my
    solution.  Perhaps mine is not the best but it's the one I created.

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -3,7 +3,7 @@ var _; // globals
 describe("About Applying What We Have Learnt", function() {
   var products;
 
-  beforeEach(function () { 
+  beforeEach(function () {
     products = [
        { name: "Sonoma", ingredients: ["artichoke", "sundried tomatoes", "mushrooms"], containsNuts: false },
        { name: "Pizza Primavera", ingredients: ["roma", "sundried tomatoes", "goats cheese", "rosemary"], containsNuts: false },
@@ -30,15 +30,31 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(productsICanEat.length).toBe(FILL_ME_IN);
+    expect(productsICanEat.length).toBe(1);
   });
 
   it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)", function () {
       var productsICanEat = [];
+      const noShrooms = function(value) {
+        return value !== "mushrooms";
+      }
+      const noNuts = products.filter(product => {
+        return !product.containsNuts
+      });
 
-      /* solve using filter() & all() / any() */
+      productsICanEat = noNuts.filter(product => {
+        if (!_.any(product.ingredients, function(ingredient) {
+          return ingredient === "mushrooms";
+        })) {
+          return product;
+        }
+      });
 
-      expect(productsICanEat.length).toBe(FILL_ME_IN);
+      return productsICanEat;
+
+      /* solve using filter() & all() / any() (Done!)*/
+
+      expect(productsICanEat.length).toBe(1);
   });
 
   /*********************************************************************************/
@@ -51,14 +67,17 @@ describe("About Applying What We Have Learnt", function() {
         sum += i;
       }
     }
-    
-    expect(sum).toBe(FILL_ME_IN);
+
+    expect(sum).toBe(233168);
   });
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
-    var sum = FILL_ME_IN;    /* try chaining range() and reduce() */
-
-    expect(233168).toBe(FILL_ME_IN);
+    var sum = _.reduce(_.range(1, 1000), function(memo, num) {
+      return (num % 3 === 0 || num % 5 === 0) ? memo + num: memo;
+      }, 0);
+     /* try chaining range() and reduce() */
+     console.log(sum);
+    expect(233168).toBe(sum);
   });
 
   /*********************************************************************************/
@@ -71,7 +90,7 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(ingredientCount['mushrooms']).toBe(FILL_ME_IN);
+    expect(ingredientCount['mushrooms']).toBe(2);
   });
 
   it("should count the ingredient occurrence (functional)", function () {
@@ -86,20 +105,20 @@ describe("About Applying What We Have Learnt", function() {
   /* UNCOMMENT FOR EXTRA CREDIT */
   /*
   it("should find the largest prime factor of a composite number", function () {
-  
+
   });
 
   it("should find the largest palindrome made from the product of two 3 digit numbers", function () {
-    
+
   });
 
   it("should find the smallest number divisible by each of the numbers 1 to 20", function () {
-      
-    
+
+
   });
 
   it("should find the difference between the sum of the squares and the square of the sums", function () {
-    
+
   });
 
   it("should find the 10001st prime", function () {
